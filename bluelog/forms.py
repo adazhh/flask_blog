e1fda9b2944b9a6619ef6ea16696640a184540b9
addzhh <13143343043@163.com>
2019-03-30 14:53:35 +0800

    v2.0

--- bluelog/forms.py
+++ bluelog/forms.py
@@ -15,24 +15,24 @@ from bluelog.models import Category
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(1, 20)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(1, 128)])
-    remember = BooleanField('Remember me')
-    submit = SubmitField('Log in')
+    username = StringField('用户名', validators=[DataRequired(), Length(1, 20)])
+    password = PasswordField('密码', validators=[DataRequired(), Length(1, 128)])
+    remember = BooleanField('记住账号')
+    submit = SubmitField('登录')
 
 
 class SettingForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(1, 70)])
-    blog_title = StringField('Blog Title', validators=[DataRequired(), Length(1, 60)])
-    blog_sub_title = StringField('Blog Sub Title', validators=[DataRequired(), Length(1, 100)])
-    about = CKEditorField('About Page', validators=[DataRequired()])
+    name = StringField('名字', validators=[DataRequired(), Length(1, 70)])
+    blog_title = StringField('博客标题', validators=[DataRequired(), Length(1, 60)])
+    blog_sub_title = StringField('博客描述', validators=[DataRequired(), Length(1, 100)])
+    about = CKEditorField('页面介绍', validators=[DataRequired()])
     submit = SubmitField()
 
 
 class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired(), Length(1, 60)])
-    category = SelectField('Category', coerce=int, default=1)
-    body = CKEditorField('Body', validators=[DataRequired()])
+    title = StringField('标题', validators=[DataRequired(), Length(1, 60)])
+    category = SelectField('分类', coerce=int, default=1)
+    body = CKEditorField('文章内容', validators=[DataRequired()])
     submit = SubmitField()
 
     def __init__(self, *args, **kwargs):
@@ -42,8 +42,8 @@ class PostForm(FlaskForm):
 
 
 class CategoryForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(1, 30)])
-    submit = SubmitField()
+    name = StringField('分类名称', validators=[DataRequired(), Length(1, 30)])
+    submit = SubmitField('')
 
     def validate_name(self, field):
         if Category.query.filter_by(name=field.data).first():
@@ -51,10 +51,10 @@ class CategoryForm(FlaskForm):
 
 
 class CommentForm(FlaskForm):
-    author = StringField('Name', validators=[DataRequired(), Length(1, 30)])
-    email = StringField('Email', validators=[DataRequired(), Email(), Length(1, 254)])
-    site = StringField('Site', validators=[Optional(), URL(), Length(0, 255)])
-    body = TextAreaField('Comment', validators=[DataRequired()])
+    author = StringField('姓名', validators=[DataRequired(), Length(1, 30)])
+    email = StringField('邮箱', validators=[DataRequired(), Email(), Length(1, 254)])
+    site = StringField('网址', validators=[Optional(), URL(), Length(0, 255)])
+    body = TextAreaField('评论', validators=[DataRequired()])
     submit = SubmitField()
 
 
@@ -65,6 +65,6 @@ class AdminCommentForm(CommentForm):
 
 
 class LinkForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(1, 30)])
-    url = StringField('URL', validators=[DataRequired(), URL(), Length(1, 255)])
+    name = StringField('链接名称', validators=[DataRequired(), Length(1, 30)])
+    url = StringField('URL地址', validators=[DataRequired(), URL(), Length(1, 255)])
     submit = SubmitField()
